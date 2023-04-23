243143b30932f8b28a58934a5f1cda993edcfc21
Peter Kompasz <kompaszbence@gmail.com>
2023-04-23 19:07:11 +0300

    frontend

---
+++ frontend/src/pages/AnalysisPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="q-pa-md">
+    <q-breadcrumbs>
+      <q-breadcrumbs-el label="Home" icon="home" to="/" />
+      <q-breadcrumbs-el label="Analysis" />
+    </q-breadcrumbs>
+
+  </q-page>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped></style>
---
+++ frontend/src/pages/SettingsPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="q-pa-md">
+    <q-breadcrumbs>
+      <q-breadcrumbs-el label="Home" icon="home" to="/" />
+      <q-breadcrumbs-el label="Settings" />
+    </q-breadcrumbs>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped></style>
