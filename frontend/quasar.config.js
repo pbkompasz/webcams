4718ea972b89ca5fb18793d9559f94c0ec4ba5fe
Peter Kompasz <kompaszbence@gmail.com>
2023-04-23 09:29:41 +0300

    Added Notify

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -105,7 +105,9 @@ module.exports = configure((/* ctx */) => ({
     // directives: [],
 
     // Quasar plugins
-    plugins: [],
+    plugins: [
+      'Notify',
+    ],
   },
 
   // animations: 'all', // --- includes all animations
